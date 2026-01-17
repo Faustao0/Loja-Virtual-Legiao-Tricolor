55053725e6209188d9bfd1081d07e5f96e0a5c3d
Bruno Manoel Toebe de Almeida <bruno.233397@edu.unipar.br>
2026-01-16 23:35:30 -0300

    .

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ btn.addEventListener('click', function (e) {
 document.addEventListener('click', function () {
   menu.style.display = 'none';
 });
+
+function adicionarAoCarrinho(produto) {
+  let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+  const produtoExistente = carrinho.find(p => p.id === produto.id);
+
+  if (produtoExistente) {
+    produtoExistente.quantidade += produto.quantidade;
+  } else {
+    carrinho.push(produto);
+  }
+
+  localStorage.setItem('carrinho', JSON.stringify(carrinho));
+
+  alert('Produto adicionado ao carrinho!');
+}
